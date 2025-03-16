ae360955e792459ef2d4e43f89b4931bdbad85e8
Rocroverss <rocroversskirtash@gmail.com>
2025-03-16 13:13:48 +0100

    delete on menu

--- Scripts/Title_scene.gd
+++ Scripts/Title_scene.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var playSound := preload("res://Audio/gui_sfx/menu_confirm.wav")
-
+var save_location = "user://save/save_state.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +11,25 @@ func _ready():
 func _input(event):
 	if event:
 		if event.is_action_pressed("jump"):
+			$Label/Timer.wait_time = $Label/Timer.wait_time/8
+			$sfxplayer.stream = playSound
+			$sfxplayer.play()
+			$musicplayer.stop()
+			yield($sfxplayer,"finished")
+			
+			get_tree().change_scene("res://Scenes/Testlevel.tscn")
+		if event.is_action_pressed("O"):
+			var file = File.new()
+			if file.file_exists(save_location):  # Check if the file exists
+				file.open(save_location, File.READ)  # Open the file
+				file.close()  # Properly close before deleting
+				var dir = Directory.new()
+				if dir.remove(save_location) == OK:
+					print("Save file deleted successfully.")
+				else:
+					print("Failed to delete save file.")
+			else:
+				print("No save file found.")
 			$Label/Timer.wait_time = $Label/Timer.wait_time/8
 			$sfxplayer.stream = playSound
 			$sfxplayer.play()
